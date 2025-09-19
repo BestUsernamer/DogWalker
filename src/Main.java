754872c5fc5c1b9330f6e3ba47f674f23863f935
BestUsernamer <haoz8810@nycstudents.net>
2025-09-19 13:13:15 -0400

    Added stuff

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,14 @@ public class Main {
         int hour = c.addDogs();
         System.out.println(c);
         DogWalker w = new DogWalker(4, c);
+        System.out.println(w.walkDogs(hour) + " dogs walked.");
+        System.out.println(c.numAvailableDogs(hour)+" dogs remaining.");
+        hour = c.addDogs();
+        for (int i=7; i < 11; i++)
+            c.addDogs();
+        System.out.println(c);
+        w = new DogWalker(3, c);
+        System.out.println("$" + w.dogWalkShift(7, 10) + ".00 earned");
     }
 
 }
